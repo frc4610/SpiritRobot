00183e554ebea0cfc4ee169923b524ce4bb6ce84
Dea-Sea <35466700+Dea-Sea@users.noreply.github.com>
2019-09-04 17:37:53 -0500

    Removed aim function and all references

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,7 +9,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -22,14 +21,12 @@ public class Shooter extends Subsystem {
   private VictorSPX shootR;//right main motor
   private VictorSPX shootLF;//Left follower motor
   private VictorSPX shootRF;//right follower motor
-  private TalonSRX aim;//currently unsused
   public Shooter()
   {
     shootL = new VictorSPX(8);
     shootR = new VictorSPX(9);
     shootLF = new VictorSPX(10);
     shootRF = new VictorSPX(11);
-    //aim = new TalonSRX(0);
     shootL.setInverted(true);
     shootLF.setInverted(true);
     shootL.follow(shootR);
@@ -37,28 +34,12 @@ public class Shooter extends Subsystem {
     shootRF.follow(shootR);
     shootL.setNeutralMode(NeutralMode.Brake);
     shootR.setNeutralMode(NeutralMode.Brake);
-    /*aim.setNeutralMode(NeutralMode.Brake);
-    aim.setSelectedSensorPosition(0,0,10);
-    aim.selectProfileSlot(0, 0);
-    aim.config_kF(0, .25, 10);//.2 - .3 to start off
-    aim.config_kP(0, .2, 10);//((Desired speed * 1023)/TargetErrItShouldBeAtDesiredSpeed)
-    aim.config_kI(0, .002, 10);//if too short, start w/ p/100 or p/10
-    aim.config_kD(0, 2, 10);//if too fast, p*10 to start*/
   }
 
   public void shoot(double speed)
   {
     shootR.set(ControlMode.PercentOutput, speed);
   }
-  /*public void aim(ControlMode mode, double value)
-  {
-    //for forward / pos move, simply state new pos, if below do pos-spos? testing required
-    aim.set(mode, value);
-  }*/
-  /*public int aEnc()
-  {
-    return aim.getSelectedSensorPosition();
-  }*/
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
